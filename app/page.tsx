a1d19d726a9689adeb7c00877ad65a594dca3d62
Shrey <Shrey33004@gmail.com>
2026-02-01 05:27:25 +0530

    Removed the parallax image effect on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Header from "@/components/Header";
 import PinnedHorizontalScroll from "@/components/InfiniteLoopPanels";
-import ParallaxImage from "@/components/ParallaxImage";
 
 
 export default function Home() {
   return (
     <>
       <Header />
-      <ParallaxImage src={"/Homepage-0.jpeg"} alt={"Hero Image"}/>
       <PinnedHorizontalScroll />
     </>
   )
